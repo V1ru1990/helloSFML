b70753d96fd267fdd00e429cd3b92207064cf9b2
v2 <grunichev.virus228@gmail.com>
2026-08-10 23:50:26 +0300

    Update window

---
+++ build/CMakeFiles/helloSFML.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloSFML.dir/link.d"
+  "CMakeFiles/helloSFML.dir/src/main.cpp.o"
+  "CMakeFiles/helloSFML.dir/src/main.cpp.o.d"
+  "helloSFML"
+  "helloSFML.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloSFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/Engine/myStuff.h
@@ -0,0 +1,4 @@
+#pragma once
+
+const int WINDOW_LENGTH = 1024;
+const int WINDOW_WIDTH = 768;
