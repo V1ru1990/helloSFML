867547a5e93d3df4f17640bd59da33da2edc0672
v1 <grunichev.virus228@gmail.com>
2026-08-17 18:27:04 +0300

    Дополнен World.h

--- include/Engine/World.h
+++ include/Engine/World.h
@@ -1,6 +1,30 @@
-#pragma once
+#pragma once 
+#include <vector>
+#include "SFML/Graphics.hpp"
+#include "Engine/Platform.h"
 
-const int WINDOW_LENGTH = 800;
-const int WINDOW_WIDTH = 600;
-const int BUFFER_ZONE = 50;
-const int TOP_POINT = -50;
+class World
+{
+    std::vector<Platform> m_platforms; //веквторы для хранения объектов класса Platform
+    int m_platformCount; //несет информацию о количестве платформ
+    int m_worldWidth, m_worldHeight; //информация об игровом поле
+    int m_scrollThreshold; //порог для скроллинга, если игрок вверх - мир вниз
+
+    sf::Texture m_platformTexture; //текстура для всех платформ
+
+    public:
+        World(); //default constructor
+        World(int width, int height, int count); //конструктор
+
+        void loadPlatforms(const char* filename); //загрузка текстуры для всех платформ
+        void update(float dy, int windowHeight); //обновление состояния мира
+        void scroll(float dy); // преместить платформы при скроллинге
+
+        void render(sf::RenderWindow& window); // отрисовка всех платформ
+
+        std::vector<Platform>& getPlatforms(); //возвращает список платформ
+
+        void generateInitialPlatforms(); //создание начальных платформ
+        void respawnPlatform(Platform& platform); //пересоздать платформу сверху
+
+};
